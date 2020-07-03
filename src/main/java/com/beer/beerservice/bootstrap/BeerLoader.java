ebea34609ce6969db01dc0e93fb39437eea6903c
Ding128 <qwseda123@gmail.com>
2020-07-03 15:45:47 -0700

    Add bootstrap

---
+++ src/main/java/com/beer/beerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,66 @@
+package com.beer.beerservice.bootstrap;
+
+import com.beer.beerservice.domain.Beer;
+import com.beer.beerservice.repository.BeerRepository;
+import com.beer.beerservice.web.model.BeerStyleEnum;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObject();
+    }
+
+    private void loadBeerObject() {
+        if (beerRepository.count() == 0) {
+            Beer b1 = Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle(BeerStyleEnum.IPA.name())
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .price(new BigDecimal("12.95"))
+                    .upc(BEER_1_UPC)
+                    .build();
+
+            Beer b2 = Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .price(new BigDecimal("12.95"))
+                    .upc(BEER_2_UPC)
+                    .build();
+
+            Beer b3 = Beer.builder()
+                    .beerName("Pinball Porter")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                    .minOnHand(12)
+                    .quantityToBrew(200)
+                    .price(new BigDecimal("12.95"))
+                    .upc(BEER_3_UPC)
+                    .build();
+
+            beerRepository.save(b1);
+            beerRepository.save(b2);
+            beerRepository.save(b3);
+
+        }
+        System.out.println("Loaded Beers: " + beerRepository.count());
+    }
+}
